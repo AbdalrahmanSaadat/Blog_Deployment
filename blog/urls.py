a17622aa8398f700a30cafd6170d61a8eab89b70
Abdalrahman Saadat <139587515+AbdalrahmanSaadat@users.noreply.github.com>
2024-03-25 21:31:08 +0200

    complete login and logout feture with the main parts of profile section

--- blog/urls.py
+++ blog/urls.py
@@ -25,5 +25,8 @@ urlpatterns = [
     path('', include('blogApp.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('profile/',user_views.profile, name='profile'),
+    path('profile/logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    
 ]
