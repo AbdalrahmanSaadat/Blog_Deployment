56819999a5b649bae5e76bfde49b2785ff51cb37
Abdalrahman Saadat <139587515+AbdalrahmanSaadat@users.noreply.github.com>
2024-03-31 00:57:13 +0200

    prepare for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ra_u&hf1r-zx3@x-^-n(f&#lu_!4ml8(0d2tu^w=co=qvfxdo!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
